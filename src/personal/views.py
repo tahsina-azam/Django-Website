2207c461015ad12720ade35a98a6a2cec5073eb1
Tahsina Bintay Azam <52793150+tahsina-azam@users.noreply.github.com>
2022-01-24 21:20:53 +0600

    passed context variables from views

--- src/personal/views.py
+++ src/personal/views.py
@@ -1,6 +1,21 @@
+from multiprocessing import context
 from django.shortcuts import render
 
 # Create your views here.
 def home_screen_view(request):
-    print(request.headers)
-    return render(request,"base.html",{})
+    # context={}
+    # context['some_string']="this is some string that I'm passing to the view"
+    # context['some_number']=12345
+    #the same 3 lines above can be written as :
+    # context = {
+    #     'some_string': "this is some string that I'm passing to the view",
+    #     'some_number': 12345
+    # }
+    list_of_values = []
+    list_of_values.append("first entry")
+    list_of_values.append("second entry")
+    list_of_values.append("third entry")
+    list_of_values.append("fourth entry")
+    context['list_of_values'] = list_of_values
+
+    return render(request,"personal/home.html",context)
