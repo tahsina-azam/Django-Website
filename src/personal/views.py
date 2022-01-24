f4f0079c71a27f3db1fe79bb4b37b4dbe078b9ac
Tahsina Bintay Azam <52793150+tahsina-azam@users.noreply.github.com>
2022-01-24 23:34:47 +0600

    using body snippets

--- src/personal/views.py
+++ src/personal/views.py
@@ -1,9 +1,10 @@
 from multiprocessing import context
 from django.shortcuts import render
+from personal.models import Question
 
 # Create your views here.
 def home_screen_view(request):
-    # context={}
+    context={}
     # context['some_string']="this is some string that I'm passing to the view"
     # context['some_number']=12345
     #the same 3 lines above can be written as :
@@ -11,12 +12,15 @@ def home_screen_view(request):
     #     'some_string': "this is some string that I'm passing to the view",
     #     'some_number': 12345
     # }
-    context={}
-    list_of_values = []
-    list_of_values.append("first entry")
-    list_of_values.append("second entry")
-    list_of_values.append("third entry")
-    list_of_values.append("fourth entry")
-    context['list_of_values'] = list_of_values
+   
+    # list_of_values = []
+    # list_of_values.append("first entry")
+    # list_of_values.append("second entry")
+    # list_of_values.append("third entry")
+    # list_of_values.append("fourth entry")
+    # context['list_of_values'] = list_of_values
+
+    questions = Question.objects.all()
+    context['questions']=questions
 
     return render(request,"personal/home.html",context)
