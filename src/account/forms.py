10f4b9340c3bf0a55d9b857bd1f6d34a21281411
Tahsina Bintay Azam <52793150+tahsina-azam@users.noreply.github.com>
2022-01-27 01:15:26 +0600

    some modifications

--- src/account/forms.py
+++ src/account/forms.py
@@ -18,7 +18,8 @@ class AccountAuthenticationForm(forms.ModelForm):
         model = Account
         fields = ('email','password')
     def clean(self):
-        email = self.cleaned_data['email']   
-        password = self.cleaned_data['password']
-        if not authenticate(email=email,password=password):
-            raise forms.ValidationError("Invalid login")
+        if self.is_valid():
+            email = self.cleaned_data['email']   
+            password = self.cleaned_data['password']
+            if not authenticate(email=email,password=password):
+                raise forms.ValidationError("Invalid login")
