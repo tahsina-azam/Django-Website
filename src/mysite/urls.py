86be03dfe311b8bd1b75d9e38e6e560b8dd4ac02
Tahsina Bintay Azam <52793150+tahsina-azam@users.noreply.github.com>
2022-01-26 21:50:37 +0600

    loggin into user account

--- src/mysite/urls.py
+++ src/mysite/urls.py
@@ -25,11 +25,13 @@ from personal.views import (
 from account.views import (
     registration_view,
     logout_view,
+    login_view,
 )
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',home_screen_view,name="home"),
     path('register/',registration_view,name="register"),
-    path('logout/',logout_view,name="logout")
+    path('logout/',logout_view,name="logout"),
+    path('login/',login_view,name="login") 
 ]
